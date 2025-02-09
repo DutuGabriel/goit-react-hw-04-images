bbe87eb60bb98166375787152ffde18bbcd12b2c
DutuGabriel <gdutu94@gmail.com>
2025-02-09 17:54:28 +0000

    Deploying to gh-pages from @ DutuGabriel/goit-react-hw-04-images@5bb9f6354ff6ec644e18067566fa6d922a331b5a 🚀

--- src/components/Loader/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import { Circles } from 'react-loader-spinner';
-
-const Loader = () => (
-  <div className="Loader-wrapper">
-    <Circles color="#00BFFF" height={60} width={60} />;
-  </div>
-);
-
-export default Loader;
